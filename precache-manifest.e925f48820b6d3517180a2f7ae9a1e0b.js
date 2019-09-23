cda7d1bb4389e35c7c9b53cf238541df8d1845ab
Ilya Cheredov <ilyacheredov@gmail.com>
2019-09-23 22:58:48 +0300

    Updates

---
+++ precache-manifest.e925f48820b6d3517180a2f7ae9a1e0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d1f84322b97e796a6b9341d0a23ee7",
+    "url": "/Ilyacheredov1/store2/index.html"
+  },
+  {
+    "revision": "ed9dbcc34607882fd42c",
+    "url": "/Ilyacheredov1/store2/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3e33255a4e4ec504240e",
+    "url": "/Ilyacheredov1/store2/static/css/main.b6bbc575.chunk.css"
+  },
+  {
+    "revision": "ed9dbcc34607882fd42c",
+    "url": "/Ilyacheredov1/store2/static/js/2.a5957677.chunk.js"
+  },
+  {
+    "revision": "3e33255a4e4ec504240e",
+    "url": "/Ilyacheredov1/store2/static/js/main.d223287e.chunk.js"
+  },
+  {
+    "revision": "cbb387aeb677133751dc",
+    "url": "/Ilyacheredov1/store2/static/js/runtime~main.ce3a9c19.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/Ilyacheredov1/store2/static/media/logo.9f7e506d.svg"
+  }
+]);
