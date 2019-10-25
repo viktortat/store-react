f0217d1a15de4e096e2fe0a720d85d53eed95c7c
Ilyacheredov1 <53446839+Ilyacheredov1@users.noreply.github.com>
2019-10-25 19:26:03 +0300

    Add files via upload

---
+++ precache-manifest.d6ff8322e85cb4e109be1aaa5adb3ea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e743f7b23d89c97123126c09ed726249",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8650770b043254e9c9d",
+    "url": "./static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "56cb5421ab7630ea2bf6",
+    "url": "./static/css/main.5908505a.chunk.css"
+  },
+  {
+    "revision": "e8650770b043254e9c9d",
+    "url": "./static/js/2.1d8c7f92.chunk.js"
+  },
+  {
+    "revision": "56cb5421ab7630ea2bf6",
+    "url": "./static/js/main.b1045b79.chunk.js"
+  },
+  {
+    "revision": "7a96b0c9e3dca4d61517",
+    "url": "./static/js/runtime~main.6e66b2b0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/store-react/precache-manifest.087d0c11f02c76bbfa84a1040869c5e1.js"
+  "./precache-manifest.d6ff8322e85cb4e109be1aaa5adb3ea5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/store-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
