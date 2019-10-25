20f52558069029fdc010053d9e7450be6415393c
Ilya Cheredov <Ilyacheredov@gamil.com>
2019-10-25 21:15:26 +0300

    Updates

---
+++ precache-manifest.ba704eb5efc3b70f93b779f58b11f2a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348b950385d536416d1dc98aaed7d758",
+    "url": "/store-react/home/index.html"
+  },
+  {
+    "revision": "e8650770b043254e9c9d",
+    "url": "/store-react/home/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c42ba913428feceaa73a",
+    "url": "/store-react/home/static/css/main.5908505a.chunk.css"
+  },
+  {
+    "revision": "e8650770b043254e9c9d",
+    "url": "/store-react/home/static/js/2.1d8c7f92.chunk.js"
+  },
+  {
+    "revision": "c42ba913428feceaa73a",
+    "url": "/store-react/home/static/js/main.af616c93.chunk.js"
+  },
+  {
+    "revision": "154675e5834a6188a5af",
+    "url": "/store-react/home/static/js/runtime~main.a856113f.js"
+  }
+]);
